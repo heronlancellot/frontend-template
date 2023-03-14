69d9bb88a527b440292661b9b3e2836f043a4597
heronlancellot <heronlancellot@id.uff.br>
2023-03-14 10:24:55 -0300

    Feat: Frontend configuration

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Header from './components/header/Header'
+import Nav from './components/nav/Nav'
+import Footer from './components/footer/Footer'
+
+const App = () => {
+  return (
+    <>
+        <Header />
+        <Nav />
+        <Footer />
+    </>
+  )
+}
+
+export default App
